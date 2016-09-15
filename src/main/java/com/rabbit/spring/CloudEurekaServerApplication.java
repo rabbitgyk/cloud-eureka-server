ea1c0304dfeb3d0f2d370947578cc7263e27af48
guoyankui <yankuiguo@gmail.com>
2016-09-15 16:14:45 +0800

    resolve the question which eureka never unregisters a service

--- src/main/java/com/rabbit/spring/CloudEurekaServerApplication.java
+++ src/main/java/com/rabbit/spring/CloudEurekaServerApplication.java
@@ -10,5 +10,6 @@ public class CloudEurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudEurekaServerApplication.class, args);
+//		DefaultEurekaServerConfig 
 	}
 }
